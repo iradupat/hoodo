8e0cdf9d59445aae286c587f9cb30b2dd23a7305
iradupat <iradupat@yahoo.fr>
2021-11-26 10:52:33 +0200

    camera

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # hoodo
 
 ## Find your lost dog with hoodo.
-=======
+
 Even though some of our team members have a phobia of dogs, our team is aware of the daily struggle of dogs in Rwanda because some of the dogs do not have shelter and most of those  are thrown stones which causes trauma and physical wounds. Our team thinks this problem requires a collective effort from all kinds of people but this is our opportunity to improve the  lives of doggs . Doggo lives matter in Rwanda..
 
  In Rwanda there are many homeless dogs and if a person who care about dogs find a wandering dog in street, the person might alert dog shelters through the app, moreover if a dog owner has lost his/her dog, the person through the app can  report to other users including dog shelters so that if found the person can know,Furthermore some owners through this app can train the dog and lastly a person can buy a puppy from the shelters after viewing the dogs they have.
--- app/src/main/java/com/example/hodoo/CreateEditPostActivity.java
+++ app/src/main/java/com/example/hodoo/CreateEditPostActivity.java
@@ -1,15 +1,33 @@
 package com.example.hodoo;
 
+import android.Manifest;
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateEditPostActivity extends AppCompatActivity {
     TextView saveBtn;
+    ImageView dogImage;
+    Uri imageUri;
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +36,41 @@ public class CreateEditPostActivity extends AppCompatActivity {
 
 
         saveBtn = findViewById(R.id.edit_create_save_btn);
+        dogImage = findViewById(R.id.dogImage);
+
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == CreateEditPostActivity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+//                            doSomeOperations();
+                            dogImage.setImageURI(imageUri);
+                        }
+                    }
+                });
+
+        dogImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                        String []  permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, 10);
+                    }else{
+                        useCamera();
+                    }
+                }else{
+                    useCamera();
+                }
+
+            }
+        });
+
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +78,48 @@ public class CreateEditPostActivity extends AppCompatActivity {
                 startActivity(postDetailIntent);
             }
         });
+    }
+
+    private void useCamera(){
+//        ContentValues values = new ContentValues();
+//        values.put(MediaStore.Images.Media.TITLE, "PIC THE DOGOO");
+//        values.put(MediaStore.Images.Media.DESCRIPTION, "PIC THE DOGOO");
+//        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageIntent);
+        startActivityForResult(imageIntent, 20);
+
+
+//        someActivityResultLauncher.launch(imageIntent);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 10:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    useCamera();
+                }else{
+                     Toast.makeText(CreateEditPostActivity.this,"Permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+
+        }
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 20){
+            Bitmap img = (Bitmap) data.getExtras().get("data");
+            dogImage.setImageBitmap(img);
+        }
     }
 }
